ea7023fa4cea5efe96e63f4b1691fd86a3e9987e
rajesh madala <ramadala@umflint.edu>
2017-07-19 19:13:05 -0500

    intial check in

---
+++ InterviewMvcApp/InterviewMvcApp/Controllers/DepartmentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InterviewMvcApp.Models;
+
+namespace InterviewMvcApp.Controllers
+{
+    public class DepartmentController : Controller
+    {
+		// GET: Department
+		public ActionResult Index()
+		{
+			Department model = new Department();
+			try
+			{
+				SampleEntities Db = new SampleEntities();
+				ViewBag.StateList = Db.Departments.ToList();
+				
+			}
+			catch(Exception ex)
+			{
+				//Logger.Log(Excption while fetching Departments from DB {0},ex);
+			}
+			
+			return View(model);
+		}
+    }
+}
---
+++ InterviewMvcApp/InterviewMvcApp/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using InterviewMvcApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterviewMvcApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+		// GET: Employee
+		public ActionResult GetEmployee(string id)
+		{
+			List<Employee> employees = null;
+			try
+			{
+				SampleEntities Db = new SampleEntities();
+				employees = (from x in Db.Employees where x.DepartmentCode == id select x).ToList();
+				return Json(employees, JsonRequestBehavior.AllowGet);
+			}
+			catch(Exception ex)
+			{
+				return Json(employees);
+				//Logger.Log("Exception while fetching Employee records {0}",ex);
+			}
+			
+		}
+    }
+}
